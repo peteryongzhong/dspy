f69ce1844e1d1aaac98f468ab30e9aef9037b12e
skylerxhu <xiaomenh@andrew.cmu.edu>
2024-11-19 13:13:31 -0500

    implemented a snapshot feature to keep track of model updates

--- dspy/teleprompt/teleprompt.py
+++ dspy/teleprompt/teleprompt.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
 class Teleprompter:
     def __init__(self):
-        pass
+        self.snapshots = []
+
+    def snapshot(self, student, version: int, complete_time: datetime):
+        self.snapshots.append((student, version, complete_time))
+
+    def get_snapshots(self):
+        return self.snapshots
+ 
